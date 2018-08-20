3ffccdcd54af9ae77bc93020bbe2382c9cee0118
sprakash1993 <sprakash1993@ymail.com>
2018-08-20 03:59:27 -0700

    CRUD Topics

--- src/main/java/com/springbootstrap/topic/TopicController.java
+++ src/main/java/com/springbootstrap/topic/TopicController.java
@@ -3,18 +3,41 @@ package com.springbootstrap.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
 
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Overview"),
-				new Topic("spring2", "Spring Framework2", "Overview2"),
-				new Topic("spring3", "Spring Framework3", "Overview3")
-				);
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void updateTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void deleteTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(topic, id);
 	}
 }
---
+++ src/main/java/com/springbootstrap/topic/TopicService.java
@@ -0,0 +1,47 @@
+package com.springbootstrap.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Overview"),
+			new Topic("spring2", "Spring Framework2", "Overview2"),
+			new Topic("spring3", "Spring Framework3", "Overview3")
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for(int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		Topic t = getTopic(id);
+		topics.remove(t);
+	}
+	
+	
+}
