02a78bd056a96923bcc1036656986c61e124bd45
sprakash1993 <sprakash1993@ymail.com>
2018-08-20 03:10:00 -0700

    initial setup

---
+++ src/main/java/com/springbootstrap/topic/TopicController.java
@@ -0,0 +1,20 @@
+package com.springbootstrap.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring", "Spring Framework", "Overview"),
+				new Topic("spring2", "Spring Framework2", "Overview2"),
+				new Topic("spring3", "Spring Framework3", "Overview3")
+				);
+	}
+}
